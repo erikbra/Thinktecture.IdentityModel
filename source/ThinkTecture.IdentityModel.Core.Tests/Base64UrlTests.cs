306c8a849a0c19f5b13f15a2c7e67647b130fbce
Erik A. Brandstadmoen <erik@brandstadmoen.net>
2014-10-20 21:38:50 +0200

    Added tests to ThinkTecture.IdentityModel.Core. Changed Base64Url a bit. Thought it would be faster. Must eat my hat.

---
+++ source/ThinkTecture.IdentityModel.Core.Tests/Base64UrlTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Thinktecture.IdentityModel;
+using Xunit;
+using FluentAssertions;
+
+namespace ThinkTecture.IdentityModel.Core.Tests
+{
+    public class Base64UrlTests
+    {
+        [Fact]
+        public void UrlEncoded_String_Does_Not_Contain_Trailing_Equal_Signs()
+        {
+            var encoded = Base64Url.Encode(GetTestData());
+            encoded.Should().NotContain("=");
+        }
+
+        [Fact]
+        public void UrlEncoded_String_Does_Not_Contain_Plus_Signs()
+        {
+            var encoded = Base64Url.Encode(GetTestData());
+            encoded.Should().NotContain("+");
+        }
+
+        [Fact]
+        public void UrlEncoded_String_Does_Not_Contain_Forward_Slash()
+        {
+            var encoded = Base64Url.Encode(GetTestData());
+            encoded.Should().NotContain("/");
+        }
+
+        [Fact]
+        public void UrlEncoded_String_Replaces_All_Special_Characters()
+        {
+            byte[] testData = GetTestData();
+
+            var encoded = Base64Url.Encode(testData);
+            var base64Encoded = Convert.ToBase64String(testData);
+
+            var expected =
+                base64Encoded
+                    .Replace("+", "-")
+                    .Replace("/", "_")
+                    .TrimEnd('=');
+
+            encoded.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void UrlDecoded_Is_Equal_To_Original()
+        {
+            byte[] testData = GetTestData();
+
+            var encoded = Base64Url.Encode(testData);
+            var decoded = Base64Url.Decode(encoded);
+
+            decoded.Should().BeEquivalentTo(testData);
+        }
+
+        private byte[] GetTestData()
+        {
+            var bytes = new byte[256];
+            for (var x = 0; x < bytes.Length; x++)
+            {
+                bytes[x] = (byte)x;
+                //Console.Write("{0:X2} ", bytes[x]);
+                //if (((x + 1) % 20) == 0) Console.WriteLine();
+            }
+
+            return bytes;
+        }
+    }
+
+}
--- source/Thinktecture.IdentityModel.Core/Base64Url.cs
+++ source/Thinktecture.IdentityModel.Core/Base64Url.cs
@@ -4,6 +4,8 @@
  */
 
 using System;
+using System.Security.Principal;
+using System.Text;
 
 namespace Thinktecture.IdentityModel
 {
@@ -12,29 +14,31 @@ namespace Thinktecture.IdentityModel
         public static string Encode(byte[] arg)
         {
             string s = Convert.ToBase64String(arg); // Standard base64 encoder
-            
-            s = s.Split('=')[0]; // Remove any trailing '='s
-            s = s.Replace('+', '-'); // 62nd char of encoding
-            s = s.Replace('/', '_'); // 63rd char of encoding
-            
-            return s;
+
+            var builder = new StringBuilder(s);
+
+            builder.Replace('+', '-');
+            builder.Replace('/', '_');
+
+            return builder.ToString().TrimEnd('=');
         }
 
         public static byte[] Decode(string arg)
         {
-            string s = arg;
-            s = s.Replace('-', '+'); // 62nd char of encoding
-            s = s.Replace('_', '/'); // 63rd char of encoding
-            
-            switch (s.Length % 4) // Pad with trailing '='s
+            var builder = new StringBuilder(arg);
+
+            builder.Replace('-', '+');
+            builder.Replace('_', '/');
+
+            switch (builder.Length % 4) // Pad with trailing '='s
             {
                 case 0: break; // No pad chars in this case
-                case 2: s += "=="; break; // Two pad chars
-                case 3: s += "="; break; // One pad char
+                case 2: builder.Append("=="); break; // Two pad chars
+                case 3: builder.Append("="); break; // One pad char
                 default: throw new Exception("Illegal base64url string!");
             }
-            
-            return Convert.FromBase64String(s); // Standard base64 decoder
+
+            return Convert.FromBase64String(builder.ToString()); // Standard base64 decoder
         }
     }
 }
